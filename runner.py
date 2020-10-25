fe44f753c7d8797c4d4eaddc3146c85628ffdc59
Mr. T <tcarduck@gmx.de>
2020-10-25 21:37:36 +0100

    Update runner.py

--- runner.py
+++ runner.py
@@ -4,17 +4,20 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO23
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO24
 # GPIO.setup(24, GPIO.OUT)  #LED to GPIO24
 
 try:
-    while True:
-         button_state = GPIO.input(23)
-         if button_state == False:
-             # GPIO.output(24, True)
-             print('Button Pressed...')
-             time.sleep(0.2)
-         else:
-             time.sleep(0.2)
-             # GPIO.output(24, False)
+        while True:
+                black_button_state = GPIO.input(23)
+                red_button_state = GPIO.input(24)
+                if black_button_state == False:
+                        print('Black Button Pressed...')
+                        time.sleep(0.2)
+                elif red_button_state == False:
+                        print('Red Button Pressed...')
+                        time.sleep(0.2)
+                else:
+                        time.sleep(0.2)
 except:
-    GPIO.cleanup()
+        GPIO.cleanup()
