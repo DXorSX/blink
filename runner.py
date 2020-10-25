950ccca6456149b5842ac02be413bb0526568dad
aCodingFool <fool@nomail.com>
2020-10-25 23:14:45 +0100

    Bugfixing

--- runner.py
+++ runner.py
@@ -8,20 +8,20 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO23
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO24
 
-try:
-        while True:
-                black_button_state = GPIO.input(23)
-                red_button_state = GPIO.input(24)
-                if black_button_state == False:
-                        print('Black Button Pressed...')
-                        blink001.rainbow_cycle_successive(pixels, wait=0.1)
-                        time.sleep(0.2)
-                elif red_button_state == False:
-                        print('Red Button Pressed...')
-                        blink001.pixels.clear()
-                        blink001.pixels.show() 
-                        time.sleep(0.2)
-                else:
-                        time.sleep(0.2)
-except:
-        GPIO.cleanup()
+#try:
+while True:
+        black_button_state = GPIO.input(23)
+        red_button_state = GPIO.input(24)
+        if black_button_state == False:
+                print('Black Button Pressed...')
+                blink001.rainbow_cycle_successive(pixels, wait=0.1)
+                time.sleep(0.2)
+        elif red_button_state == False:
+                print('Red Button Pressed...')
+                blink001.pixels.clear()
+                blink001.pixels.show() 
+                time.sleep(0.2)
+        else:
+                time.sleep(0.2)
+#except:
+#        GPIO.cleanup()
