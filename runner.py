6eb35fb612f5c66fab1d7b4e9dcf5f4c788f80e0
aCodingFool <fool@nomail.com>
2020-10-25 23:17:39 +0100

    Bugfixing

--- runner.py
+++ runner.py
@@ -1,12 +1,21 @@
 import RPi.GPIO as GPIO
+import Adafruit_WS2801
+import Adafruit_GPIO.SPI as SPI
 import time
 import subprocess
 import blink001
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO23
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO24
+
+# Configure the count of pixels:
+PIXEL_COUNT = 219
+ 
+# Alternatively specify a hardware SPI connection on /dev/spidev0.0:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
 
 #try:
 while True:
