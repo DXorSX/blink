db2c210d06230e0104c20e6df5adee172cdcbc33
aCodingFool <fool@nomail.com>
2020-10-26 02:20:57 +0100

    .

--- blink001.py
+++ blink001.py
@@ -44,7 +44,7 @@ def rainbow_cycle_successive(pixels, wait=0.1):
         if wait > 0:
             time.sleep(wait)
  
-def rainbow_cycle(pixels, wait=0.005):
+def rainbow_cycle(pixels, wait=0.05):
     for j in range(256): # one cycle of all 256 colors in the wheel
         for i in range(pixels.count()):
             pixels.set_pixel(i, wheel(((i * 256 // pixels.count()) + j) % 256) )
