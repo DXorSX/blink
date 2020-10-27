a4d8e05418b8869b8fab0556b13a2303a3db1f85
aCodingFool <fool@nomail.com>
2020-10-27 14:37:53 +0100

    .

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ apt install git python-pip -y
 pip install adafruit-ws2801
 
 mkdir /home/pi/logs && chown pi.pi /home/pi/logs && chmod 755 /home/pi/logs
-echo "@reboot date >>/home/pi/logs/cronlog 2>&1 && cd /home/pi && rm -rf /home/pi/blink && until ping -c 2 github.com ; do sleep 1; done ; echo "Server responding" >>/home/pi/logs/cronlog 2>&1 && git clone https://github.com/DXorSX/blink.git >>/home/pi/logs/cronlog 2>&1 && cd /home/pi/blink && python2.7 runner.py >>/home/pi/logs/runner.log #Do not modify: runner.py start" | sudo crontab -u pi -
+echo "@reboot date >>/home/pi/logs/cron.log 2>&1 && cd /home/pi && rm -rf /home/pi/blink && until ping -c 2 github.com ; do sleep 1; done ; echo "Server responding" >>/home/pi/logs/cron.log 2>&1 && git clone https://github.com/DXorSX/blink.git >>/home/pi/logs/cron.log 2>&1 && cd /home/pi/blink && python2.7 -u runner.py >>/home/pi/logs/runner.log #Do not modify: runner.py start" | sudo crontab -u pi -
